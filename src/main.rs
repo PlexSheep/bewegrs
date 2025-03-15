c0a737873c9a3ead80438f2c585eca062228ce56
Christoph J. Scherr <software@cscherr.de>
2025-03-15 01:32:54 +0100

    feat: counters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sfml::{
+    cpp::FBox,
     graphics::{
         Color, CustomShape, CustomShapePoints, RenderTarget, RenderWindow, Shape, Transformable,
     },
@@ -6,6 +7,8 @@ use sfml::{
     window::{Event, Key, Style, VideoMode},
     SfResult,
 };
+
+const MAX_FPS: u32 = 60;
 
 #[derive(Clone, Copy)]
 pub struct TriangleShape;
@@ -43,6 +46,55 @@ fn hue_time(t: f32) -> Color {
     Color::rgb(r as u8, g as u8, b as u8)
 }
 
+/// lazy fields get updated every [FRAME_MAX] frames
+#[derive(Debug)]
+struct Counters {
+    /// frame counter
+    pub frames: u64,
+    /// frame counter lazy
+    pub lframes: u64,
+    /// seconds counter
+    pub seconds: f32,
+    /// seconds counter lazy
+    pub lseconds: f32,
+
+    /// actually keeps track of time
+    pub clock: FBox<Clock>,
+}
+
+impl Counters {
+    fn start() -> SfResult<Self> {
+        Ok(Counters {
+            clock: Clock::start()?,
+            lframes: 0,
+            frames: 0,
+            seconds: 0.0,
+            lseconds: 0.0,
+        })
+    }
+    fn tick(&mut self) {
+        self.seconds = self.clock.elapsed_time().as_seconds();
+        self.frames += 1;
+
+        if self.frames % MAX_FPS as u64 == 0 {
+            println!("seconds: {:.2}", self.seconds);
+            println!("frames: {}", self.frames);
+            println!("FPS: {}", self.fps());
+            self.lseconds = self.seconds;
+            self.lframes = self.frames;
+        }
+    }
+    fn dframes(&self) -> u64 {
+        self.frames - self.lframes
+    }
+    fn dseconds(&self) -> f32 {
+        self.seconds - self.lseconds
+    }
+    fn fps(&self) -> f32 {
+        self.dframes() as f32 / self.dseconds()
+    }
+}
+
 fn main() -> SfResult<()> {
     let mut window = RenderWindow::new(
         VideoMode::new(1000, 600, 1),
@@ -50,8 +102,8 @@ fn main() -> SfResult<()> {
         Style::DEFAULT,
         &Default::default(),
     )?;
-    let clock = Clock::start()?;
-    window.set_vertical_sync_enabled(true);
+    let mut counter = Counters::start()?;
+    window.set_framerate_limit(MAX_FPS);
 
     let mut shape = CustomShape::new(Box::new(TriangleShape));
     shape.set_position((400., 300.));
@@ -69,13 +121,13 @@ fn main() -> SfResult<()> {
             }
         }
 
-        let t = clock.elapsed_time().as_seconds();
+        counter.tick();
 
-        shape.set_rotation(t.sin().abs() * 360.0);
-        let scale = t.cos().abs();
+        shape.set_rotation(counter.seconds.sin().abs() * 360.0);
+        let scale = counter.seconds.cos().abs();
         shape.set_scale(scale);
-        shape.set_fill_color(hue_time(t));
-        shape.set_outline_color(hue_time(t / 2.0));
+        shape.set_fill_color(hue_time(counter.seconds));
+        shape.set_outline_color(hue_time(counter.seconds / 2.0));
         window.clear(Color::BLACK);
         window.draw(&shape);
         window.display();
