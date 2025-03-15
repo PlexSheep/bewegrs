aa58422e9fc029ae7134233b452dee19a28ca170
Christoph J. Scherr <software@cscherr.de>
2025-03-15 18:15:03 +0100

    feat: add logging with tracing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use sfml::{
     window::{Event, Key, Style, VideoMode},
     SfResult,
 };
+use tracing::trace;
 
 use self::{
     counters::{Counters, MAX_FPS},
@@ -24,7 +25,27 @@ pub mod counters;
 pub mod shapes;
 pub mod ui;
 
+fn setup() {
+    // construct a subscriber that prints formatted traces to stdout
+    let subscriber = tracing_subscriber::fmt()
+        .with_max_level(
+            #[cfg(debug_assertions)]
+            tracing::Level::TRACE,
+            #[cfg(not(debug_assertions))]
+            tracing::Level::INFO,
+        )
+        .without_time()
+        .with_file(false)
+        .with_target(false)
+        .with_writer(std::io::stderr)
+        .finish();
+    // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber).expect("could not setup logger");
+    trace!("set up the logger");
+}
+
 fn main() -> SfResult<()> {
+    setup();
     let video = VideoMode::new(WINDOW_WIDTH, WINDOW_HEIGHT, 16);
 
     let mut window = RenderWindow::new(video, "Custom shape", Style::DEFAULT, &Default::default())?;
